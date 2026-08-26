589ab325b3d29512f14e75abaa3f6bb5c7482840
Huzaifa Baig <148705865+huzvfa@users.noreply.github.com>
2026-08-27 00:21:31 +0500

    Update AddTransactionSheet.kt

--- app/src/main/java/com/khata/finance/ui/AddTransactionSheet.kt
+++ app/src/main/java/com/khata/finance/ui/AddTransactionSheet.kt
@@ -24,6 +24,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.khata.finance.data.TxnType
 import com.khata.finance.viewmodel.FinanceViewModel
 
